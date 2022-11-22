fd9da30dea49763486ae1de8a47766b1cd281e0a
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2022-11-22 16:28:27 +0530

    Create Doubly linkedlist.c
    This is doubly linked list. The difference between singly and doubly linked list is that you can insert element at beginning, last of the list as well.

---
+++ Data Structres/Doubly linkedlist.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *head){
+    struct Node *ptr = head;
+    do{
+        printf("Element is %d\n", ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
+}
+
+struct Node * insertAtFirst(struct Node *head, int data){
+    struct Node * ptr = (struct Node *) malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node * p = head->next;
+    while(p->next != head){
+        p = p->next;
+    }
+    // At this point p points to the last node of this circular linked list
+
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+
+}
+
+int main(){
+    
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    // Link first and second nodes
+    head->data = 2;
+    head->next = second;
+
+    // Link second and third nodes
+    second->data = 4;
+    second->next = third;
+
+    // Link third and fourth nodes
+    third->data = 6;
+    third->next = fourth;
+
+    // Terminate the list at the third node
+    fourth->data = 8;
+    fourth->next = head;
+
+    printf("Linked list before insertion (Insrt at begining)\n");
+    linkedListTraversal(head);
+    head = insertAtFirst(head, 14);
+    head = insertAtFirst(head, 16);
+    head = insertAtFirst(head, 18);
+    printf("Linked list after insertion (Insert at last)\n");
+    linkedListTraversal(head);
+    return 0;
+}
