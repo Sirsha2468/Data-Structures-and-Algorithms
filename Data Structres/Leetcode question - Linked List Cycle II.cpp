59f5e5e3927709487d7c809b7e1c59126fbe1575
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2023-03-09 22:53:09 +0530

    Create Leetcode question - Linked List Cycle II.cpp

---
+++ Data Structres/Leetcode question - Linked List Cycle II.cpp
@@ -0,0 +1,48 @@
+// Problem statement: Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+// Testcase 1 - Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+// Constraints:
+
+// The number of the nodes in the list is in the range [0, 104].
+// -105 <= Node.val <= 105
+// pos is -1 or a valid index in the linked-list.
+
+// Solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
+
+        if(head == NULL || head -> next == NULL) 
+        return NULL;
+        
+        while(fast -> next != NULL && fast -> next -> next != NULL) {
+            slow = slow -> next;
+            fast = fast-> next -> next;
+
+            if(slow == fast) {
+                while(slow != entry) {
+                    slow = slow -> next;
+                    entry = entry ->next;
+                }
+                return entry;
+            }
+        }
+        return NULL;
+    }
+};
